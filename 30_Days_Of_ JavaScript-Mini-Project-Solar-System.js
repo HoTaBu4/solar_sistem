61781390a82c2c56e8fc710e129bf4a0f5644bce
volodya <hohiop09@gmail.com>
2022-11-21 09:31:10 +0100

    beta -v1.1

--- 30_Days_Of_ JavaScript-Mini-Project-Solar-System.js
+++ 30_Days_Of_ JavaScript-Mini-Project-Solar-System.js
@@ -1,13 +1,14 @@
 const mass = ["/img/platens/mercury.png","/img/platens/earth.png","/img/platens/moon.png","/img/platens/jupiter.png","/img/platens/neptune.png","/img/platens/uranus.png","/img/platens/venus.png","/img/platens/saturn.png"]
-let url;
+
 let num;
 
 document.addEventListener('click',(event) =>{
     const targetMenu = document.getElementById('menu')
-
-    if(event.target.closest('.image')){
-        targetMenu.classList.toggle('visible')
-    }
+    if(targetMenu){
+        if(event.target.closest('.image')){
+            targetMenu.classList.toggle('visible')
+        }
+    }   
 
     if(event.target.closest('.container_menu')){
             const text = event.target.innerHTML
@@ -19,43 +20,34 @@ document.addEventListener('click',(event) =>{
 })       
      
 function tap(){
-    let text = document.getElementById('text');
+    let textInput = document.getElementById('text');
     let logs = document.getElementById('log');
     let planetName = document.getElementById('span')
+    let error = document.getElementsByClassName('.errors')
 
      
     if(typeof logs.innerHTML !== null){
         logs.innerHTML = ''
         logs.style.cssText=''
     }
+
+    if(error){
+        logs.classList.remove('errors')
+    }
 //errors
-    if(isNaN(+text.value) || typeof(num) == 'undefined' || text.value == ''){
+    if(isNaN(+textInput.value) || typeof(num) == 'undefined' || textInput.value == ''){
 
             let div = document.createElement('div')
-            let childDiv = document.createElement('div')
-                
-                logs.style.cssText =`
-                min-width:100%;
-                background-color:rgb(230, 230, 230,0.3);
-                max-width: 1000px;
-                width: 100%;
-                margin: 20px auto; 
-                padding: 30px 10rem;
-                `
+                div.setAttribute('class','errors_background')
 
-                div.style.cssText =`
-                opacity:60%;
-                background-color: rgb(185, 185, 185);
-                width:100%;
-                text-align:center;
-                font-size:40px;
-                padding:20px;
-                ` 
+            let childDiv = document.createElement('div')
+            
+        logs.classList.add('errors')
 
             logs.prepend(div)
             div.prepend(childDiv)
 
-            if(isNaN(+text.value) || text.value == '')childDiv.innerHTML = 'write a number'
+            if(isNaN(+textInput.value) || textInput.value == '')childDiv.innerHTML = 'write a number'
             if(typeof(num) == 'undefined')childDiv.innerHTML = 'choose a planet'
         }else{
 //result
@@ -64,28 +56,16 @@ function tap(){
         image.setAttribute('alt','image')
 
         const divLog = document.createElement('div')
+                divLog.setAttribute('class','log_info')
             
             divLog.innerHTML = `
-            <div style="
-                    align-items: center;
-                    display: flex;
-                    gap: 10px;
-                    justify-content:center;
-                    font-size: 20px;">
+            <div  class="output_text">
                     the weigth of the object on a 
                 <h3>
                     ${planetName.innerText}
                 </h3>
             </div>
-            <div style="
-                background-color:grey;  
-                height: 120px;
-                width: 120px;
-                border-radius: 50%;
-                display: flex;
-                align-items: center;
-                justify-content:center;
-            ">
+            <div class="output_result">
                 ${'math'}
             </div>`
 
@@ -99,15 +79,6 @@ function tap(){
             flex-direction:row;
             padding:50px 20px;
             gap:20px;
-            `
-            divLog.style.cssText=`
-            background-color: rgb(185, 185, 185,0.3);
-            display:flex;
-            flex-direction:column;
-            align-items: center;
-            color:white;
-            height:250px;
-            width:100%
             `
     }
 } 
